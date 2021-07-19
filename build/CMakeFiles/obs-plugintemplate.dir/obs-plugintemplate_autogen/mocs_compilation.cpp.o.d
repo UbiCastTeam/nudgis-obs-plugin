98d4e2a077eb054b5503ea10b4886a1834a36903
Matthieu Ribeaucoup-Nahon <matthieuribeaucoup-nahon@MacBook-Pro-de-Matthieu.local>
2021-07-19 10:21:39 +0200

    manage build folder w/ gitignore

--- build/CMakeFiles/obs-plugintemplate.dir/obs-plugintemplate_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/obs-plugintemplate.dir/obs-plugintemplate_autogen/mocs_compilation.cpp.o: \
-  /Users/matthieuribeaucoup-nahon/Documents/ubicast/obs-plugintemplate/build/obs-plugintemplate_autogen/mocs_compilation.cpp
