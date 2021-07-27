dbaa5ef4038e5af36d44ff4d07a6a7b1dacc90c5
matthieu <matthieu@matthieu-mac>
2021-07-27 12:11:13 +0200

    main: adding header and Settings class

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -2,13 +2,46 @@
 Nudgis-OBS-Plugin
 Copyright (C) 2021 Ubicast 
 */
-
+ 
 #include <obs-module.h>
 #include "plugin-macros.generated.h"
+#include "plugin-main.hpp"
 #include <QMainWindow>
 #include <QAction>
 #include <QtWidgets>
 #include <iostream>
+
+/*-----------------------------------------------------------------------------------------------*/
+
+
+
+/*-----------------------------------------------------------------------------------------------*/
+
+NudgisSettings::NudgisSettings() : QWidget()
+{
+    /*blog(LOG_INFO, "Plugin Settings Opened (version %s)", PLUGIN_VERSION);
+    QWidget * window = new QWidget();
+    window->resize(320, 240);
+    window->show();
+    window->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));*/
+}
+
+NudgisSettings::~NudgisSettings() 
+{
+
+}
+
+void NudgisSettings::openSettings() 
+{
+
+}
+
+void NudgisSettings::closeSettings() 
+{
+
+}
+
+/*-----------------------------------------------------------------------------------------------*/
 
 extern "C"
 {
@@ -31,11 +64,10 @@ void openWindow()
     window->resize(320, 240);
     window->show();
     window->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));
-
 }
-
 bool obs_module_load()
 {
+
     //std::cout << "Hello World 2, This is my CPP OBS plugin!\n";
     blog(LOG_INFO, "Nudgis plugin loaded successfully (version %s)", PLUGIN_VERSION);
     QAction * menu_action = (QAction*) obs_frontend_add_tools_menu_qaction("Nudgis Plugin Settings");
