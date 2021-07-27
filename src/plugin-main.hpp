311b0940845b04a8bcc6373b1523cc38165b5afd
matthieu <matthieu@matthieu-mac>
2021-07-27 12:11:13 +0200

    main: adding header and Settings class

---
+++ src/plugin-main.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <QWidget>
+#include <QString>
+
+class NudgisSettings : public QWidget {
+
+    Q_OBJECT
+
+    public: 
+
+        NudgisSettings();
+        ~NudgisSettings();
+        void openSettings();
+        void closeSettings();
+
+    private:
+
+
+};
+
+
+
+
