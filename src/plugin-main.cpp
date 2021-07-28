5efd15e9ffa12bbebbf91d5866cf9443047c8456
matthieu <matthieu@matthieu-mac>
2021-07-28 14:04:25 +0200

    Error with class NudgisSettings

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -7,23 +7,19 @@ Copyright (C) 2021 Ubicast
 #include "plugin-macros.generated.h"
 #include "plugin-main.hpp"
 #include <QMainWindow>
+#include <QApplication>
 #include <QAction>
 #include <QtWidgets>
 #include <iostream>
 
+
+/*NUDGIS SETTINGS METHODS DEFINITION*/
 /*-----------------------------------------------------------------------------------------------*/
+static NudgisSettings *settingsWindow = nullptr;
 
-
-
-/*-----------------------------------------------------------------------------------------------*/
-
-NudgisSettings::NudgisSettings() : QWidget()
+NudgisSettings::NudgisSettings() : QWidget(nullptr)
 {
-    /*blog(LOG_INFO, "Plugin Settings Opened (version %s)", PLUGIN_VERSION);
-    QWidget * window = new QWidget();
-    window->resize(320, 240);
-    window->show();
-    window->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));*/
+    blog(LOG_INFO, "Plugin Settings Opened (version %s)", PLUGIN_VERSION);
 }
 
 NudgisSettings::~NudgisSettings() 
@@ -31,22 +27,29 @@ NudgisSettings::~NudgisSettings()
 
 }
 
-void NudgisSettings::openSettings() 
+
+/*HANDLING OBS EVENTS*/
+/*-----------------------------------------------------------------------------------------------*/
+
+/*static void obs_event(enum obs_frontend_event event, void *)
 {
+	if (event == OBS_FRONTEND_EVENT_EXIT) 
+    {
+		delete settingsWindow;
+	} 
+    else if (event == OBS_FRONTEND_EVENT_SCENE_COLLECTION_CLEANUP) 
+    {
+        
+	}
+}*/
 
-}
-
-void NudgisSettings::closeSettings() 
-{
-
-}
-
+/*LOAD AND UNLOAD OF THE PLUGIN*/
 /*-----------------------------------------------------------------------------------------------*/
 
 extern "C"
 {
-EXPORT void *obs_frontend_get_main_window(void);
-EXPORT void *obs_frontend_add_tools_menu_qaction(const char *name);
+    EXPORT void *obs_frontend_get_main_window(void);
+    EXPORT void *obs_frontend_add_tools_menu_qaction(const char *name);
 }
 
 OBS_DECLARE_MODULE()
@@ -60,11 +63,21 @@ const char *obs_module_name(void)
 void openWindow()
 {
     blog(LOG_INFO, "Plugin Settings Opened (version %s)", PLUGIN_VERSION);
-    QWidget * window = new QWidget();
-    window->resize(320, 240);
-    window->show();
-    window->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));
+    /*if (!settingsWindow) 
+    {
+        settingsWindow = new NudgisSettings();
+        settingsWindow->resize(320, 240);
+        settingsWindow->show();
+        settingsWindow->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));
+	} 
+    else 
+    {
+        settingsWindow->show();
+        settingsWindow->setWindowTitle(QApplication::translate("toplevel", "Nudgis Plugin Settings"));
+        settingsWindow->raise();
+	}*/
 }
+
 bool obs_module_load()
 {
 
@@ -73,6 +86,7 @@ bool obs_module_load()
     QAction * menu_action = (QAction*) obs_frontend_add_tools_menu_qaction("Nudgis Plugin Settings");
     blog(LOG_INFO, "[%s] Menu entry for Settings added", obs_module_name());
     menu_action->connect(menu_action, &QAction::triggered, openWindow);
+    //obs_frontend_add_event_callback(obs_event, nullptr);
     return true;
 }
 
