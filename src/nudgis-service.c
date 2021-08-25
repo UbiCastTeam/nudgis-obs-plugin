01bacc6f21c5551f6a1d44732b051a8cf640f6c8
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-08-25 12:48:40 +0200

    workaround - force use of nudgis service
    this commit is a workaround to force OBS to use the nudgis service on
    startup.
    
    Adding a log to each obs_service_info callback call
    Using obs_frontend_save_streaming_service to update local configuration
    profile with the default nudgis service
    Added include obs-frontend-api.h for the declaration of the prototype
    of obs_frontend_save_streaming_service and the declaration of
    obs_frontend_event enums

--- src/nudgis-service.c
+++ src/nudgis-service.c
@@ -7,23 +7,28 @@ struct nudgis {
 
 static const char *nudgis_name(void *unused)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	UNUSED_PARAMETER(unused);
 	return obs_module_text("Nudgis");
 }
 
 static void nudgis_update(void *data, obs_data_t *settings)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	struct nudgis *service =  data;
 
 	bfree(service->server);
 	bfree(service->key);
 
-	service->server = bstrdup(obs_data_get_string(settings, "server"));
-	service->key = bstrdup(obs_data_get_string(settings, "key"));
+	//service->server = bstrdup(obs_data_get_string(settings, "server"));
+	//service->key = bstrdup(obs_data_get_string(settings, "key"));
+	service->server = bstrdup("fake server");
+	service->key = bstrdup("fake key");
 }
 
 static void nudgis_destroy(void *data)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	struct nudgis *service =  data;
 
 	bfree(service->server);
@@ -33,6 +38,7 @@ static void nudgis_destroy(void *data)
 
 static void *nudgis_create(obs_data_t *settings, obs_service_t *service)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	struct nudgis *data = bzalloc(sizeof(struct nudgis));
 	nudgis_update(data, settings);
 
@@ -42,11 +48,12 @@ static void *nudgis_create(obs_data_t *settings, obs_service_t *service)
 
 static bool nudgis_initialize(void *data, obs_output_t *output)
 {
-	
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 }
 
 static obs_properties_t *nudgis_properties(void *unused)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	UNUSED_PARAMETER(unused);
 
 	obs_properties_t *ppts = obs_properties_create();
@@ -62,12 +69,14 @@ static obs_properties_t *nudgis_properties(void *unused)
 
 static const char *nudgis_url(void *data)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	struct nudgis *service =  data;
 	return "mystreamurl";
 }
 
 static const char *nudgis_key(void *data)
 {
+    blog(LOG_INFO, "Enter in %s", __FUNCTION__);
 	struct nudgis *service =  data;
 	return "mystreamkey";
 }
