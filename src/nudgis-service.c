aaf8ce301091ea252ef36d1c5f5f96d7efd1fbe4
Matthieu <matthieuribeaucoup@orange.fr>
2021-08-26 16:37:09 +0200

    nudgis-service: fix typo and warnings

--- src/nudgis-service.c
+++ src/nudgis-service.c
@@ -1,5 +1,59 @@
 #include <obs-module.h>
 #include "nudgis-service.h"
+#include <curl/curl.h>
+
+#define DEFAULT_DATA "api_key=no_key&title=MonStream2"
+
+typedef struct _buffer_data_t
+{
+    char * buffer;
+    size_t len;
+}buffer_data_t;
+
+static size_t write_callback(void *data, size_t size, size_t nmemb, buffer_data_t * buffer_data)
+{
+    size_t size_new_data = size * nmemb;
+    buffer_data->buffer = realloc(buffer_data->buffer,buffer_data->len + size_new_data);
+    if (buffer_data->buffer)
+    {
+        memcpy(buffer_data->buffer + buffer_data->len,data,size_new_data);
+        buffer_data->len += size_new_data;
+    }
+    return size_new_data;
+}
+
+int liveprepare(char * data)
+{
+    CURL *curl;
+    buffer_data_t buffer_data = {.buffer = NULL, .len = 0};
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+
+    if(curl) 
+    {
+
+    curl_easy_setopt(curl, CURLOPT_URL, "https://beta.ubicast.net/api/v2/lives/prepare/");
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer_data);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+    res = curl_easy_perform(curl);
+
+    if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+    curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    if (buffer_data.buffer != NULL)
+    {
+        printf("%.*s\n",(int)buffer_data.len,buffer_data.buffer);
+        free(buffer_data.buffer);
+    }
+
+    return 0;
+}
 
 struct nudgis {
 	char *server, *key;
@@ -52,6 +106,9 @@ static bool nudgis_initialize(void *data, obs_output_t *output)
     (void)output;
     (void)data;
     blog(LOG_INFO, "Enter in %s", __func__);
+	char * httpData = DEFAULT_DATA;
+	printf("start with: '%s'\n",httpData);
+    liveprepare(httpData);
     return true;
 }
 
@@ -60,7 +117,7 @@ static const char *nudgis_url(void *data)
     blog(LOG_INFO, "Enter in %s", __func__);
 	struct nudgis *service =  data;
     (void)service;
-	return "mystreamurl";
+	return "rtmp://beta.ubicast.net/FE6ZrfWAUCuRCJ9xAFHZfqzeX86EA5";
 }
 
 static const char *nudgis_key(void *data)
@@ -68,7 +125,7 @@ static const char *nudgis_key(void *data)
     blog(LOG_INFO, "Enter in %s", __func__);
 	struct nudgis *service =  data;
     (void)service;
-	return "mystreamkey";
+	return "beta_msuser_H8TLhntzEYGqO0g9n7vYCoXs4wQ1l6_720";
 }
 
 
