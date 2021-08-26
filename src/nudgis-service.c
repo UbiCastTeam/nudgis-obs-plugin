bc4cb4af918d706c193fb8eded1e46542929eaf7
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-08-26 19:23:39 +0200

    wip

--- src/nudgis-service.c
+++ src/nudgis-service.c
@@ -2,6 +2,7 @@
 #include "nudgis-service.h"
 #include <curl/curl.h>
 #include <jansson.h>
+#include <unistd.h>
 
 #define FAKE_API_KEY ""
 #define FAKE_TITLE "MonStream2"
@@ -49,26 +50,6 @@ void process_prepare_response(const buffer_data_t * buffer_data,nudgis_t * nudgi
 {
     if (buffer_data != NULL && nudgis != NULL)
     {
-        /* {
-        "message" : "Live stream prepared.",
-        "oid" : "l1261a6e411d9qg1vdwy",
-        "service" : "rtmp",
-        "stream_id" : "beta_msuser_cabnOn3hpIpesK71UOubqGpCWlf61x",
-        "streams" : [
-            {
-                "audio_bitrate" : 128000,
-                "framerate" : 30,
-                "height" : 720,
-                "publish_uri" : "rtmp://beta.ubicast.net/FE6ZrfWAUCuRCJ9xAFHZfqzeX86EA5/beta_msuser_cabnOn3hpIpesK71UOubqGpCWlf61x_720",
-                "server_uri" : "rtmp://beta.ubicast.net/FE6ZrfWAUCuRCJ9xAFHZfqzeX86EA5",
-                "stream_id" : "beta_msuser_cabnOn3hpIpesK71UOubqGpCWlf61x_720",
-                "video_bitrate" : 1000000,
-                "width" : 1280
-            }
-        ],
-        "success" : true
-        } */
-
 
         if (buffer_data->buffer != NULL)
         {        
@@ -86,20 +67,14 @@ void process_prepare_response(const buffer_data_t * buffer_data,nudgis_t * nudgi
             blog(LOG_INFO,"server_uri_str: %s",server_uri_str);
             blog(LOG_INFO,"stream_id_str: %s",stream_id_str);
 
-            if (nudgis != NULL)
-            {
-                if (nudgis->key != NULL)
-                    free(nudgis->key);
-                nudgis->key = strdup(stream_id_str);
+            bfree(nudgis->key);
+            nudgis->key = bstrdup(stream_id_str);
 
-                if (nudgis->server != NULL)
-                    free(nudgis->server);
-                nudgis->server = strdup(server_uri_str);
+            bfree(nudgis->server);
+            nudgis->server = bstrdup(server_uri_str);
 
-                if (nudgis->oid != NULL)
-                    free(nudgis->oid);
-                nudgis->oid = strdup(oid_str);
-            }
+            bfree(nudgis->oid);
+            nudgis->oid = bstrdup(oid_str);
 
             json_decref(root);
         }
@@ -142,67 +117,6 @@ int send_request(const char * url,char * data,buffer_data_t * buffer_data)
     return result;
 }
 
-/* int liveprepare(char * data,nudgis_t * nudgis)
-{
-    CURL *curl;
-    buffer_data_t buffer_data = {.buffer = NULL, .len = 0};
-    CURLcode res;
-    curl_global_init(CURL_GLOBAL_ALL);
-    curl = curl_easy_init();
-
-    if(curl) 
-    {
-
-    curl_easy_setopt(curl, CURLOPT_URL, "https://beta.ubicast.net/api/v2/lives/prepare/");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer_data);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-    res = curl_easy_perform(curl);
-
-    if(res != CURLE_OK)
-        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-    curl_easy_cleanup(curl);
-    }
-
-    curl_global_cleanup();
-
-
-    if (buffer_data.buffer != NULL)
-    {
-        printf("%.*s\n",(int)buffer_data.len,buffer_data.buffer);
-        json_t *root;        
-        root = json_loadb(buffer_data.buffer,buffer_data.len,0,NULL);
-        json_t *streams = json_object_get(root,"streams");
-        json_t *stream= json_array_get(streams,0);
-        json_t *server_uri = json_object_get(stream,"server_uri");
-        const char * server_uri_str = json_string_value(server_uri);
-        json_t *stream_id = json_object_get(stream,"stream_id");
-        const char * stream_id_str = json_string_value(stream_id);
-
-        blog(LOG_INFO,"server_uri_str: %s",server_uri_str);
-        blog(LOG_INFO,"stream_id_str: %s",stream_id_str);
-
-        if (nudgis != NULL)
-        {
-            if (nudgis->key != NULL)
-                free(nudgis->key);
-            nudgis->key = strdup(stream_id_str);
-
-            if (nudgis->server != NULL)
-                free(nudgis->server);
-            nudgis->server = strdup(server_uri_str);
-        }
-
-        
-
-        json_decref(root);
-
-        free(buffer_data.buffer);
-    }
-
-    return 0;
-} */
-
 static const char *nudgis_name(void *unused)
 {
     blog(LOG_INFO, "Enter in %s", __func__);
@@ -232,6 +146,7 @@ static void nudgis_destroy(void *data)
 
 	bfree(service->server);
 	bfree(service->key);
+    bfree(service->oid);
 	bfree(service);
 }
 
@@ -245,25 +160,38 @@ static void *nudgis_create(obs_data_t *settings, obs_service_t *service)
 	return data;
 }
 
-#define BUFFER_SIZE  2048
+static char * bstrdup_printf(const char * format, ... )
+{
+    char * result = NULL;
+    va_list args;
+    va_start (args, format);
+    int len_alloc = vsnprintf(NULL,0,format, args) + 1;
+    result = bzalloc(len_alloc);
+    va_start (args, format);
+    vsnprintf(result,len_alloc,format, args);
+    va_end (args);
+    return result;
+}
 
 static bool nudgis_initialize(void *data, obs_output_t *output)
 {
     (void)output;
-    (void)data;
     nudgis_t * nudgis = data;
     blog(LOG_INFO, "Enter in %s", __func__);
-    buffer_data_t buffer_data = {.len=0, .buffer=NULL};
-    char payload[BUFFER_SIZE];
-    snprintf(payload,BUFFER_SIZE,FMT_PREPARE_REQUEST,FAKE_API_KEY,FAKE_TITLE,FAKE_CHANNEL);
-    blog(LOG_INFO,"PREPARE_URL payload: %s",payload);
-    send_request(PREPARE_URL,payload,&buffer_data);
+    buffer_data_t buffer_data = {0};
+    char * payload_prepare_url = bstrdup_printf(FMT_PREPARE_REQUEST,FAKE_API_KEY,FAKE_TITLE,FAKE_CHANNEL);
+    blog(LOG_INFO,"payload_prepare_url: %s",payload_prepare_url);
+    send_request(PREPARE_URL,payload_prepare_url,&buffer_data);
     process_prepare_response(&buffer_data,nudgis);
     buffer_data_free(&buffer_data);
 
-    snprintf(payload,BUFFER_SIZE,FMT_START_REQUEST,FAKE_API_KEY,nudgis->oid);
-    blog(LOG_INFO,"START_URL payload: %s",payload);
-    send_request(START_URL,payload,&buffer_data);
+    char * payload_start_url = bstrdup_printf(FMT_START_REQUEST,FAKE_API_KEY,nudgis->oid);
+    blog(LOG_INFO,"payload_start_url: %s",payload_start_url);
+    send_request(START_URL,payload_start_url,&buffer_data);
+    blog(LOG_INFO,"start_request_response: %.*s",(int)buffer_data.len,buffer_data.buffer);
+
+    bfree(payload_prepare_url);
+    bfree(payload_start_url);
 
     return true;
 }
@@ -273,16 +201,12 @@ static const char *nudgis_url(void *data)
     blog(LOG_INFO, "Enter in %s", __func__);
 	struct nudgis *service =  data;
     return service->server;
-    //(void)service;
-	//return "rtmp://beta.ubicast.net/FE6ZrfWAUCuRCJ9xAFHZfqzeX86EA5";
 }
 
 static const char *nudgis_key(void *data)
 {
     blog(LOG_INFO, "Enter in %s", __func__);
 	struct nudgis *service =  data;
-    //(void)service;
-	//return "beta_msuser_H8TLhntzEYGqO0g9n7vYCoXs4wQ1l6_720";
     return service->key;
 }
 
