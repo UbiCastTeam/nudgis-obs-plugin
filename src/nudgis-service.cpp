7dd7b1cbeda08a77f64e562578e8c174991a4e1b
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-08-31 11:41:33 +0200

    nudgis-service: add debug log from GetRemoteFile

--- src/nudgis-service.cpp
+++ src/nudgis-service.cpp
@@ -105,8 +105,11 @@ bool GetRemoteFile(
 
 bool GetRemoteFile(const char *url, std::string &str, const char *postData = nullptr)
 {
+    bool result = false;
     string error = {};
-    return GetRemoteFile(url,str,error,nullptr,nullptr,"",postData);
+    result =  GetRemoteFile(url,str,error,nullptr,nullptr,"",postData);
+    blog(LOG_DEBUG, "GetRemoteFile response: %s", str.c_str());
+    return result;
 }
 
 bool GetRemoteFile(const char *url, const char *postData = nullptr)
@@ -133,7 +136,6 @@ static bool nudgis_initialize(void *data, obs_output_t *output)
     string response_url = {};
     GetRemoteFile(prepare_url.str().c_str(),response_url,prepare_postdata.str().c_str());
 
-    blog(LOG_INFO,"response_url: %s",response_url.c_str());
     process_prepare_response(response_url,nudgis);
 
     ostringstream start_url("");
