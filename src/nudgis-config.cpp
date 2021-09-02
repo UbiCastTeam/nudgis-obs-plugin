27d19c578b450348aecaacd5ae541b68afff18eb
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-09-02 18:32:02 +0200

    project: apply clang format

--- src/nudgis-config.cpp
+++ src/nudgis-config.cpp
@@ -4,14 +4,14 @@
 #include <obs.hpp>
 #include <obs-utils.hpp>
 
-#define DEF_URL            "https://beta.ubicast.net"
-#define DEF_API_KEY        ""
-#define DEF_STREAM_TITLE   "Title"
+#define DEF_URL "https://beta.ubicast.net"
+#define DEF_API_KEY ""
+#define DEF_STREAM_TITLE "Title"
 #define DEF_STREAM_CHANNEL "Channel"
-#define DEF_VOD_TITLE      ""
-#define DEF_VOD_CHANNEL    ""
+#define DEF_VOD_TITLE ""
+#define DEF_VOD_CHANNEL ""
 
-#define SIZEOF_PATH        512
+#define SIZEOF_PATH 512
 
 static NudgisConfig currentNudgisConfig;
 
@@ -25,41 +25,40 @@ NudgisConfig::NudgisConfig()
     this->vod_channel = DEF_VOD_CHANNEL;
 }
 
-void NudgisConfig::load(const char * filename)
+void NudgisConfig::load(const char *filename)
 {
     char path[SIZEOF_PATH];
-    GetProfilePath(path,SIZEOF_PATH,filename);
-    
+    GetProfilePath(path, SIZEOF_PATH, filename);
+
     obs_data_t *data = obs_data_create_from_json_file_safe(path, "bak");
-    
-    if (data != NULL)
-    {
+
+    if (data != NULL) {
         obs_data_set_default_string(data, "url", DEF_URL);
         this->url = obs_data_get_string(data, "url");
-        
+
         obs_data_set_default_string(data, "api_key", DEF_API_KEY);
         this->api_key = obs_data_get_string(data, "api_key");
-        
+
         obs_data_set_default_string(data, "stream_title", DEF_STREAM_TITLE);
         this->stream_title = obs_data_get_string(data, "stream_title");
-        
+
         obs_data_set_default_string(data, "stream_channel", DEF_STREAM_CHANNEL);
         this->stream_channel = obs_data_get_string(data, "stream_channel");
-        
+
         obs_data_set_default_string(data, "vod_title", DEF_VOD_TITLE);
         this->vod_title = obs_data_get_string(data, "vod_title");
-        
+
         obs_data_set_default_string(data, "vod_channel", DEF_VOD_CHANNEL);
         this->vod_channel = obs_data_get_string(data, "vod_channel");
-        
+
         obs_data_release(data);
     }
 }
 
-void NudgisConfig::save(const char * filename)
+void NudgisConfig::save(const char *filename)
 {
     char path[SIZEOF_PATH];
-    GetProfilePath(path,SIZEOF_PATH,filename);
+    GetProfilePath(path, SIZEOF_PATH, filename);
 
     obs_data_t *data = obs_data_create();
 
@@ -76,11 +75,10 @@ void NudgisConfig::save(const char * filename)
     obs_data_release(data);
 }
 
-NudgisConfig * NudgisConfig::GetCurrentNudgisConfig()
+NudgisConfig *NudgisConfig::GetCurrentNudgisConfig()
 {
     static bool first_call = true;
-    if (first_call)
-    {
+    if (first_call) {
         currentNudgisConfig.load();
         first_call = false;
     }
