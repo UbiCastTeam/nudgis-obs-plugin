c18c911fb73d547bcd252efa2b28590c35efd691
Paul Goulpié <paul.goulpie@ubicast.eu>
2021-09-15 14:40:39 +0200

    obs-utils: add GetRemoteFile

--- src/obs/obs-utils.hpp
+++ src/obs/obs-utils.hpp
@@ -2,4 +2,15 @@
 
 #pragma once
 
+#include <vector>
+#include <string>
+
 const char *obs_frontend_get_current_profile_path(const char *filename);
+
+bool GetRemoteFile(
+        const char *url, std::string &str, std::string &error,
+        long *responseCode = nullptr, const char *contentType = nullptr,
+        std::string request_type = "", const char *postData = nullptr,
+        std::vector<std::string> extraHeaders = std::vector<std::string>(),
+        std::string *signature = nullptr, int timeoutSec = 0,
+        bool fail_on_error = true);
