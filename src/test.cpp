c6397c92a95396116890993d8f89feca03d38949
Paul Goulpié <paul.goulpie@smile.fr>
2022-04-21 18:59:43 +0200

    test: added test program for video upload

---
+++ src/test.cpp
@@ -0,0 +1,11 @@
+#include "nudgis-service.hpp"
+
+#include <QVersionNumber>
+
+int main(int argc, char *argv[])
+{
+    //nudgis_upload_file("/home/pagou/2022-02-21 16-13-01.mkv");
+    //nudgis_upload_file("/home/pagou/Dev/ubicast/mediaserver-client/test.txt");
+    nudgis_upload_file("/home/pagou/Dev/ubicast/mediaserver-client/test.mkv");
+    return 0;
+}
