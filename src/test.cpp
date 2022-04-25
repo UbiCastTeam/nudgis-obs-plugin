8c57f00f1ee3877aa7fe628c127c5cffd64ccbea
Paul Goulpié <paul.goulpie@smile.fr>
2022-04-25 09:29:37 +0200

    test: using a parameter to specify the file to upload

--- src/test.cpp
+++ src/test.cpp
@@ -1,11 +1,18 @@
 #include "nudgis-service.hpp"
 
-#include <QVersionNumber>
+#include <iostream>
+
+#define DEF_FILENAME "/home/pagou/Dev/ubicast/mediaserver-client/test.mkv"
 
 int main(int argc, char *argv[])
 {
-    //nudgis_upload_file("/home/pagou/2022-02-21 16-13-01.mkv");
-    //nudgis_upload_file("/home/pagou/Dev/ubicast/mediaserver-client/test.txt");
-    nudgis_upload_file("/home/pagou/Dev/ubicast/mediaserver-client/test.mkv");
+    const char * filename = DEF_FILENAME;
+    if (argc > 1)
+        filename = argv[1];
+
+    std::cout << "start with " << std::endl
+              << "  filename: " << filename << std::endl;
+
+    nudgis_upload_file(filename);
     return 0;
 }
