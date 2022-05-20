2df68ebb67ac996cd6bbba9a07618033bcb76f29
Paul Goulpié <paul.goulpie@smile.fr>
2022-05-20 15:44:14 +0200

    macos: replace install script with pkg installer

--- CI/macos/04_package_plugin.sh
+++ CI/macos/04_package_plugin.sh
@@ -20,22 +20,14 @@ package_obs_plugin() {
     install -m 0755 ${BUILD_FOLDER}/${PLUGIN_NAME}.so  ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER}/Applications/OBS.app/Contents/PlugIns/
     install -m 0644 ${BUILD_FOLDER}/../data/locale/* ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER}/Applications/OBS.app/Contents/Resources/data/obs-plugins/${PLUGIN_NAME}/locale/
 
-    tar -C ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER} -caf ${BUILD_FOLDER}/${PLUGIN_NAME}.tar.gz Applications/OBS.app/Contents/PlugIns/ Applications/OBS.app/Contents/Resources/data/obs-plugins/${PLUGIN_NAME}/locale
-
-    cat > ${BUILD_FOLDER}/${PLUGIN_NAME}-install.command <<EOF
+    cat > ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER}/Applications/OBS.app/Contents/Resources/data/obs-plugins/${PLUGIN_NAME}/${PLUGIN_NAME}-uninstall.command <<EOF
 #! /usr/bin/env bash
-cd \$(dirname "\$0")
-tar -C / -xvf ${PLUGIN_NAME}.tar.gz
+sudo rm -rf /Applications/OBS.app//Contents/PlugIns/${PLUGIN_NAME}.so /Applications/OBS.app//Contents/Resources/data/obs-plugins/${PLUGIN_NAME}
+sudo pkgutil --forget ${PKGID}
 EOF
-    chmod a+x ${BUILD_FOLDER}/${PLUGIN_NAME}-install.command
+    chmod a+x ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER}/Applications/OBS.app/Contents/Resources/data/obs-plugins/${PLUGIN_NAME}/${PLUGIN_NAME}-uninstall.command
 
-    cat > ${BUILD_FOLDER}/${PLUGIN_NAME}-uninstall.command <<EOF
-#! /usr/bin/env bash
-rm -rf /Applications/OBS.app//Contents/PlugIns/${PLUGIN_NAME}.so /Applications/OBS.app//Contents/Resources/data/obs-plugins/${PLUGIN_NAME}
-EOF
-    chmod a+x ${BUILD_FOLDER}/${PLUGIN_NAME}-uninstall.command
-
-    tar -C ${BUILD_FOLDER} -caf ${BUILD_FOLDER}/${PLUGIN_NAME}-package.tar.gz ${PLUGIN_NAME}.tar.gz ${PLUGIN_NAME}-uninstall.command ${PLUGIN_NAME}-install.command
+    pkgbuild --identifier ${PKGID} --root ${BUILD_FOLDER}/${BUNDLE_IMAGE_FOLDER} ${BUILD_FOLDER}/${PLUGIN_NAME}-macos-${ARCH}_${PLUGIN_VERSION}.pkg
 }
 
 package-plugin-standalone() {
@@ -46,6 +38,9 @@ package-plugin-standalone() {
     PLUGIN_NAME="${PRODUCT_NAME:-nudgis-obs-plugin}"
     BUILD_FOLDER="${BUILD_FOLDER:-build}"
     BUNDLE_IMAGE_FOLDER="${BUNDLE_IMAGE_FOLDER:-bundle-image}"
+    PKGID="${PKGID:-eu.ubicast.nudgis-obs-plugin.pkg}"
+    ARCH="${ARCH:-x86_64}"
+    PLUGIN_VERSION="${PLUGIN_VERSION:-27.2.4_1.0.0}"
 
     package_obs_plugin
 }
@@ -55,8 +50,10 @@ print_usage() {
             "-h, --help                     : Print this help\n" \
             "-q, --quiet                    : Suppress most build process output\n" \
             "-v, --verbose                  : Enable more verbose build process output\n" \
+            "-a, --architecture             : Specify build architecture (default: x86_64, alternative: arm64)\n" \
             "--build-dir                    : Specify alternative build directory (default: build)\n" \
-            "--bundle-image-folder          : Specify alternative bundle-image directory (default: bundle-image)\n"
+            "--bundle-image-folder          : Specify alternative bundle-image directory (default: bundle-image)\n" \
+            "--pkgid                        : Specify alternative pkgid (default: eu.ubicast.nudgis-obs-plugin.pkg)\n"
 }
 
 package-plugin-main() {
@@ -66,8 +63,11 @@ package-plugin-main() {
                 -h | --help ) print_usage; exit 0 ;;
                 -q | --quiet ) export QUIET=TRUE; shift ;;
                 -v | --verbose ) export VERBOSE=TRUE; shift ;;
+                -a | --architecture ) ARCH="${2}"; shift 2 ;;
                 --build-dir ) BUILD_FOLDER="${2}"; shift 2 ;;
                 --bundle-image-folder ) BUNDLE_IMAGE_FOLDER="${2}"; shift 2 ;;
+                --pkgid ) PKGID="${2}"; shift 2 ;;
+                --plugin-version ) PLUGIN_VERSION="${2}"; shift 2 ;;
                 -- ) shift; break ;;
                 * ) break ;;
             esac
